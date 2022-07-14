813842834d495a08c3da335ee37e69c71935f38c
Vatsal Parmar <67117845+vatsalparmar007@users.noreply.github.com>
2022-07-14 15:14:14 +0530

    Add files via upload

---
+++ C Chapter 2/arithmeticinstructions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{
+  float a = 4;
+  float b = 7.0;
+
+  float z = a/b;
+  
+  
+  
+  printf("The value of z is %f \n",4.0/7);
+  printf("the value of 2 power 2 is %f",pow(2,4));
+  return 0;
+  
+}
---
+++ C Chapter 2/operator_precedence.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+  
+//https://youtu.be/ZSPZob_1TOk?t=7764//
+  return 0;  
+}
