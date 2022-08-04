9c762c7c7471a1d78d827d2529d4eada68577224
Vatsal Parmar <67117845+vatsalparmar007@users.noreply.github.com>
2022-08-04 15:01:23 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int year;
+	printf("Enter Year\n");
+	scanf("%d", &year);
+
+
+	if (year%4==0)
+	{
+		printf("%d is leap year\n",year);
+	}
+	else{
+		printf("%d is not a leap year\n",year );
+	}
+
+	return 0;
+}
